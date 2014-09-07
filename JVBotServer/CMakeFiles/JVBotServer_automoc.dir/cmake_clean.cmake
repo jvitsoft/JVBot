7935799b6755dfe0aa3e27c5bfef51b99c6e33d8
jvitsoft <dianasexy.ytb@gmail.com>
2014-09-07 11:19:52 +0100

    s

--- JVBotServer/CMakeFiles/JVBotServer_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "JVBotServer_automoc.cpp"
-  "CMakeFiles/JVBotServer_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/JVBotServer_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
